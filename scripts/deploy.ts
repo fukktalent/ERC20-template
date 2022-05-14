049641fc2b5161bfa60385f43a43233790c4baaf
roma <roma.klabukov@gmail.com>
2022-05-15 00:47:25 +0300

    tests refactor

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,13 +8,13 @@ const TOKEN_NAME: string = process.env.TOKEN_NAME || 'sample token';
 const TOKEN_SYMBOL: string = process.env.TOKEN_SYMBOL || 'SAMPLE';
 
 async function main() {
-  const [owner] = await ethers.getSigners();
-  const voting = await new ERC20Token__factory(owner).deploy(TOKEN_NAME, TOKEN_SYMBOL);
-  await voting.deployed();
-  console.log("Voting deployed to:", voting.address);
+    const [owner] = await ethers.getSigners();
+    const voting = await new ERC20Token__factory(owner).deploy(TOKEN_NAME, TOKEN_SYMBOL);
+    await voting.deployed();
+    console.log("Voting deployed to:", voting.address);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
